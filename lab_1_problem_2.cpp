e14ac99a5b0f1513b9fc7e1093d605fb86e42995
Tushar Jain <53609001+XEnItAnE-DSK-tPP@users.noreply.github.com>
2019-08-04 21:25:43 +0530

    Update lab_1_problem_2.cpp

--- lab_1_problem_2.cpp
+++ lab_1_problem_2.cpp
@@ -1,16 +1,56 @@
 #include<bits/stdc++.h>
 using namespace std;
 //declaring  function
+int isSmaller(string str1, string str2) 
+{ 
+    // Calculate lengths of both string 
+    int n1 = str1.length(), n2 = str2.length(); 
+  
+    if (n1 < n2) 
+        return 1; 
+    if (n1 > n2) 
+        return 0; 
+  
+    for (int i=0; i<n1; i++) 
+    { 
+        if (str1[i] < str2[i]) 
+            return 1; 
+        else if (str1[i] > str2[i]) 
+            return 0; 
+    } 
+    return 1; 
+}
 string  findDifference(string str1,  string str2)
 {
+    string stemp1,  stemp2;
+    int k;
+    for(k=0;k<str1.length();k++)
+    {
+        if(str1[i]!=0)
+        {
+            break;
+        }
+    }
+    for(;k<str1.length();k++)
+    {
+        stemp1.push_back(str1[i]);
+    }
+    
+    for(k=0;k<str2.length();k++)
+    {
+        if(str2[i]!=0)
+        {
+            break;
+        }
+    }
+    for(;k<str2.length();k++)
+    {
+        stemp2.push_back(str2[i]);
+    }
     //to make  sure that string 2 is larger
-    if(str1.length()>str2.length())
+    if(isSmaller(stemp1,stemp2)==0)
     {
         swap(str1, str2);
-    }
-    if((str1.length()==str2.length())&&(str1[0]>str2[0]))
-    {
-        swap(str1,str2);
     }
     string str="";
     int n1=str1.length(),  n2=str2.length(), diff=n2-n1, borrow=0;
